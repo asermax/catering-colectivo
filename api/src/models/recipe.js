fc49bd3eaf12aa6abe188dc40f7f81688dbbbbe6
Agustin Carrasco <asermax@gmail.com>
2017-06-03 01:15:16 -0300

    recipe: added mongoose validations

--- api/src/models/recipe.js
+++ api/src/models/recipe.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose'
 
 const RecipeSchema = mongoose.Schema({
-  ingredient: String,
-  description: String,
-  quantity: Number,
-  unit: String,
-  proportion: Number,
+  ingredient: { type: String, required: true, minlength: 1 },
+  description: { type: String, required: true, minlength: 1 },
+  quantity: { type: Number, required: true, min: 1 },
+  unit: { type: String, required: true, minlength: 1 },
+  proportion: { type: Number, required: true, min: 1 },
   creationDate: { type: Date, default: Date.now },
 })
 
