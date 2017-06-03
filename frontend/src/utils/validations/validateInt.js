eb8f273ac6112ab366da7a23ade741b2e9843a05
Agustin Carrasco <asermax@gmail.com>
2017-06-03 01:14:11 -0300

    validate int: sent default on validation, added min parameter

--- frontend/src/utils/validations/validateInt.js
+++ frontend/src/utils/validations/validateInt.js
@@ -1,8 +1,8 @@
-const validateInt = (callback, defaultValue = null) => (
-  (value) => {
+const validateInt = (callback) => (
+  (value, defaultValue = null, min = null) => {
     value = parseInt(value)
 
-    if (!isNaN(value)) {
+    if (!isNaN(value) && (min == null || value >= min)) {
       callback(value)
     } else {
       callback(defaultValue)
