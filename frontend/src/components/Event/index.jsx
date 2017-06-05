7d31cc3291d921d9180ec4c1fbe7bc71a7fd3b9a
Agustin Carrasco <asermax@gmail.com>
2017-06-04 22:29:07 -0300

    event: added event component

---
+++ frontend/src/components/Event/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import moment from 'moment'
+import Card from 'components/Card'
+import styles from './styles.scss'
+
+const Event = (props) => (
+  <Card
+    onEdit={props.onEdit}
+    onDelete={props.onDelete}
+    leftAction={{ icon: 'pencil', callback: props.onEdit }}
+    rightAction={{ icon: 'trash', callback: props.onDelete }}
+  >
+    <div className={styles.eventContent}>
+      <h1 className="title">
+        {props.organization}
+      </h1>
+      <h2 className="subtitle">
+        para {props.amountPeople} personas.
+      </h2>
+      <div className={`content ${styles.eventDetails}`}>
+        <div className={styles.eventDescription}>
+          {props.description}
+        </div>
+        <small>
+          {moment(props.date).calendar()}
+        </small>
+      </div>
+    </div>
+  </Card>
+)
+
+Event.propTypes = {
+  organization: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  date: PropTypes.instanceOf(Date),
+  amountPeople: PropTypes.number,
+  onEdit: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+}
+
+export default Event
