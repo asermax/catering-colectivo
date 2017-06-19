aefce273b7fe39295627b658b3f688bc5572c5d9
Agustin Carrasco <asermax@gmail.com>
2017-06-18 21:19:06 -0300

    card: refactor component using recompose

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,52 +1,77 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import {
+  compose, branch, renderComponent, renderNothing, withProps, flattenProp,
+} from 'recompose'
 import styles from './styles.scss'
 
-const renderAction = (position, action) => (
+const Action = ({ position, disabled, callback, icon }) => (
   <div className={`level-${position}`}>
     <button
       className={`level-item button is-white is-paddingless ${styles.action}`}
-      disabled={action.disabled}
-      onClick={action.callback}
+      disabled={disabled}
+      onClick={callback}
     >
       <span className="icon is-small">
-        <i className={`fa fa-${action.icon}`} />
+        <i className={`fa fa-${icon}`} />
       </span>
     </button>
   </div>
 )
 
-const renderActions = (props) => {
-  let leftAction, rightAction, actions
-
-  if (props.leftAction != null) {
-    leftAction = renderAction('left', props.leftAction)
-  }
-
-  if (props.rightAction != null) {
-    rightAction = renderAction('right', props.rightAction)
-  }
-
-  if (leftAction != null || rightAction != null) {
-    actions = (
-      <nav
-        className={classNames('level', styles.actions, { [styles.isHidden]: props.hiddenActions })}
-      >
-        {leftAction != null ? leftAction : <div className="level-left" />}
-        {rightAction}
-      </nav>
-    )
-  }
-
-  return actions
+Action.propTypes = {
+  position: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  callback: PropTypes.func.isRequired,
+  icon: PropTypes.string.isRequired,
 }
 
-const Card = (props) => (
+const LeftAction = compose(
+  branch(
+    (props) => props.action == null,
+    renderComponent(() => <div className="level-left" />),
+  ),
+  flattenProp('action'),
+  withProps({ position: 'left' }),
+)(Action)
+
+const RightAction = compose(
+  branch(
+    (props) => props.action == null,
+    renderNothing,
+  ),
+  flattenProp('action'),
+  withProps({ position: 'right' }),
+)(Action)
+
+
+const Actions = ({ hiddenActions, leftAction, rightAction }) => (
+  <nav className={classNames('level', styles.actions, { [styles.isHidden]: hiddenActions })}>
+    <LeftAction action={leftAction} />
+    <RightAction action={rightAction} />
+  </nav>
+)
+
+Actions.propTypes = {
+  hiddenActions: PropTypes.bool,
+  leftAction: PropTypes.shape({
+    disabled: PropTypes.bool,
+    callback: PropTypes.func.isRequired,
+    icon: PropTypes.string.isRequired,
+  }),
+  rightAction: PropTypes.shape({
+    disabled: PropTypes.bool,
+    callback: PropTypes.func.isRequired,
+    icon: PropTypes.string.isRequired,
+  }),
+}
+
+const Card = ({ children, ...actions }) => (
   <div className={`card ${styles.card}`}>
     <div className={`card-content ${styles.cardContent}`}>
-      {props.children}
-      {renderActions(props)}
+      {children}
+      <Actions {...actions} />
     </div>
   </div>
 )
