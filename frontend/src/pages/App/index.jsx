25bb88929e1ee87ab010cc64446ba0ae71a8b239
Agustin Carrasco <asermax@gmail.com>
2017-06-18 23:49:47 -0300

    app: refactor using recompose

--- frontend/src/pages/App/index.jsx
+++ frontend/src/pages/App/index.jsx
@@ -3,57 +3,54 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Link from 'redux-first-router-link'
 import classNames from 'classnames'
+import { compose, mapProps } from 'recompose'
 import { EVENT_GALLERY, RECIPE_GALLERY, RECIPE_EDIT, RECIPE_ADD } from 'pages/routes'
 import EventsGallery from 'pages/EventsGallery'
 import RecipeGallery from 'pages/RecipeGallery'
 
-const App = ({ page }) => {
-  const Page = pages[page]
-
-  return (
-    <main>
-      <header className="hero is-primary">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title is-1">
-              Catering Colectivo
-            </h1>
+const App = ({ Page }) => (
+  <main>
+    <header className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title is-1">
+            Catering Colectivo
+          </h1>
+        </div>
+      </div>
+    </header>
+    <nav className="nav">
+      <div className="container">
+        <div className="nav-left">
+          <div className="nav-item">
+            <Link
+              href={{ type: EVENT_GALLERY }}
+              className={classNames('is-tab', { 'is-active': Page === EventsGallery })}
+            >
+              Eventos
+            </Link>
+          </div>
+          <div className="nav-item">
+            <Link
+              href={{ type: RECIPE_GALLERY }}
+              className={classNames('is-tab', { 'is-active': Page === RecipeGallery })}
+            >
+              Recetas
+            </Link>
           </div>
         </div>
-      </header>
-      <nav className="nav">
-        <div className="container">
-          <div className="nav-left">
-            <div className="nav-item">
-              <Link
-                href={{ type: EVENT_GALLERY }}
-                className={classNames('is-tab', { 'is-active': Page === EventsGallery })}
-              >
-                Eventos
-              </Link>
-            </div>
-            <div className="nav-item">
-              <Link
-                href={{ type: RECIPE_GALLERY }}
-                className={classNames('is-tab', { 'is-active': Page === RecipeGallery })}
-              >
-                Recetas
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
-      <section className="section">
-        <div className="container">
-          {Page ? <Page /> : null}
-        </div>
-      </section>
-    </main>
-  )
-}
+      </div>
+    </nav>
+    <section className="section">
+      <div className="container">
+        {Page ? <Page /> : null}
+      </div>
+    </section>
+  </main>
+)
 
 App.propTypes = {
-  page: PropTypes.string.isRequired,
+  Page: PropTypes.func,
 }
 
 const pages = {
@@ -67,4 +64,9 @@ const mapStateToProps = (state) => ({
   page: state.location.type,
 })
 
-export default connect(mapStateToProps)(App)
+const enhancer = compose(
+  connect(mapStateToProps),
+  mapProps(({ page }) => ({ Page: pages[page] })),
+)
+
+export default enhancer(App)
