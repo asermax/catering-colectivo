0a829ef2041080a0bb8b741487063e40f14de978
Agustin Carrasco <asermax@gmail.com>
2017-06-18 23:40:43 -0300

    editable recipe: refactor using recompose

--- frontend/src/components/EditableRecipe/index.jsx
+++ frontend/src/components/EditableRecipe/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { compose, withProps, withHandlers } from 'recompose'
 import ContentEditable from 'react-contenteditable'
 import { coalesce, validations } from 'utils'
 import Card from 'components/Card'
@@ -7,45 +8,25 @@ import VerticalCenteredContent from 'components/VerticalCenteredContent'
 import baseStyles from '../Recipe/styles.scss'
 import styles from './styles.scss'
 
-const handleKeyDown = (event, props) => {
-  if (event.key === 'Enter') {
-    event.preventDefault()
-    props.onSave()
-  } else if (event.key === 'Escape') {
-    props.onCancel()
-  }
-}
-
-const handleKeyUp = (event) => {
-  if (event.key === 'Tab') {
-    const range = document.createRange()
-    range.selectNodeContents(event.target)
-    const selection = window.getSelection()
-    selection.removeAllRanges()
-    selection.addRange(range)
-  }
-}
-
-const handleBlur = () => window.getSelection().removeAllRanges()
-
-const isValid = (props) => Object.values(props).every((prop) => prop != null)
-
-const Recipe = (props) => (
+const EditableRecipe = ({
+  ingredient, description, quantity, unit, proportion, isValid, onChange, onSave, onCancel,
+  handleBlur, handleKeyDown, handleKeyUp,
+}) => (
   <Card
     hiddenActions={false}
-    leftAction={{ icon: 'check', callback: props.onSave, disabled: !isValid(props) }}
-    rightAction={{ icon: 'ban', callback: props.onCancel }}
+    leftAction={{ icon: 'check', callback: onSave, disabled: !isValid }}
+    rightAction={{ icon: 'ban', callback: onCancel }}
   >
     <div className={baseStyles.recipeContent}>
       <h1 className="title">
         <ContentEditable
           className={styles.contentEditable}
           placeholder="Ingrediente"
-          html={coalesce(props.ingredient, '')}
+          html={coalesce(ingredient, '')}
           onChange={(event) => (
-            validations.str((value) => props.onChange({ ingredient: value }))(event.target.value)
+            validations.str((value) => onChange({ ingredient: value }))(event.target.value)
           )}
-          onKeyDown={(event) => handleKeyDown(event, props)}
+          onKeyDown={handleKeyDown}
           onKeyUp={handleKeyUp}
           onBlur={handleBlur}
         />
@@ -54,11 +35,11 @@ const Recipe = (props) => (
         <ContentEditable
           className={styles.contentEditable}
           placeholder="descripción"
-          html={coalesce(props.description, '')}
+          html={coalesce(description, '')}
           onChange={(event) => (
-            validations.str((value) => props.onChange({ description: value }))(event.target.value)
+            validations.str((value) => onChange({ description: value }))(event.target.value)
           )}
-          onKeyDown={(event) => handleKeyDown(event, props)}
+          onKeyDown={handleKeyDown}
           onKeyUp={handleKeyUp}
           onBlur={handleBlur}
         />
@@ -68,13 +49,13 @@ const Recipe = (props) => (
           <ContentEditable
             tagName="span"
             className={styles.contentEditable}
-            html={coalesce(props.quantity, '').toString()}
+            html={coalesce(quantity, '').toString()}
             onChange={(event) => (
-              validations.int(
-                (value) => props.onChange({ quantity: value })
-              )(event.target.value, props.quantity, 1)
+              validations.int((value) => onChange({ quantity: value }))(
+                event.target.value, quantity, 1,
+              )
             )}
-            onKeyDown={(event) => handleKeyDown(event, props)}
+            onKeyDown={handleKeyDown}
             onKeyUp={handleKeyUp}
             onBlur={handleBlur}
           />
@@ -82,12 +63,11 @@ const Recipe = (props) => (
           <ContentEditable
             tagName="span"
             className={styles.contentEditable}
-            html={coalesce(props.unit, '')}
+            html={coalesce(unit, '')}
             onChange={(event) => (
-              validations.str((value) =>
-                props.onChange({ unit: value }))(event.target.value)
+              validations.str((value) => onChange({ unit: value }))(event.target.value)
             )}
-            onKeyDown={(event) => handleKeyDown(event, props)}
+            onKeyDown={handleKeyDown}
             onKeyUp={handleKeyUp}
             onBlur={handleBlur}
           />
@@ -95,13 +75,13 @@ const Recipe = (props) => (
           <ContentEditable
             tagName="span"
             className={styles.contentEditable}
-            html={coalesce(props.proportion, '').toString()}
+            html={coalesce(proportion, '').toString()}
             onChange={(event) => (
-              validations.int(
-                (value) => props.onChange({ proportion: value })
-              )(event.target.value, props.proportion, 1)
+              validations.int((value) => onChange({ proportion: value }))(
+                event.target.value, proportion, 1,
+              )
             )}
-            onKeyDown={(event) => handleKeyDown(event, props)}
+            onKeyDown={handleKeyDown}
             onKeyUp={handleKeyUp}
             onBlur={handleBlur}
           />
@@ -112,15 +92,48 @@ const Recipe = (props) => (
   </Card>
 )
 
-Recipe.propTypes = {
+EditableRecipe.propTypes = {
   ingredient: PropTypes.string,
   description: PropTypes.string,
   quantity: PropTypes.number,
   unit: PropTypes.string,
   proportion: PropTypes.number,
+  isValid: PropTypes.bool.isRequired,
   onChange: PropTypes.func,
   onSave: PropTypes.func,
   onCancel: PropTypes.func,
+  handleKeyDown: PropTypes.func.isRequired,
+  handleKeyUp: PropTypes.func.isRequired,
+  handleBlur: PropTypes.func.isRequired,
 }
 
-export default Recipe
+const enhancer = compose(
+  withProps((props) => ({
+    isValid: Object.values(props).every((prop) => prop != null),
+  })),
+  withHandlers({
+    handleKeyDown: ({ isValid, onSave, onCancel }) => (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault()
+
+        if (isValid) {
+          onSave()
+        }
+      } else if (event.key === 'Escape') {
+        onCancel()
+      }
+    },
+    handleKeyUp: () => (event) => {
+      if (event.key === 'Tab') {
+        const range = document.createRange()
+        range.selectNodeContents(event.target)
+        const selection = window.getSelection()
+        selection.removeAllRanges()
+        selection.addRange(range)
+      }
+    },
+    handleBlur: () => () => (window.getSelection().removeAllRanges()),
+  }),
+)
+
+export default enhancer(EditableRecipe)
