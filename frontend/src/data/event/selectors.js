cc63c6e8c7cb3d12a170fc5c024be05afb5b2d33
Agustin Carrasco <asermax@gmail.com>
2017-07-02 23:47:02 -0300

    event: obtain recipes for the event details

--- frontend/src/data/event/selectors.js
+++ frontend/src/data/event/selectors.js
@@ -1,22 +1,30 @@
 import { createSelector } from 'reselect'
+import { getRecipes } from 'data/recipe/selectors'
 
 export const getEvents = (state) => state.event.list
 export const getEditingId = (state) => state.event.edit.editingId
 export const getEditingEvent = createSelector(
-  [ getEvents, getEditingId ],
-  (events, id ) => {
+  [ getEvents, getEditingId, getRecipes ],
+  (events, id, recipes) => {
     let result = null
 
     if (id != null) {
-      const match = events.find((recipe) => recipe._id === id)
+      const match = events.find((event) => event._id === id)
 
       if (match != null) {
         let { __typename, _id, creationDate, ...event } = match
-        result = event
+
+        // populate recipes
+        result = {
+          ...event,
+          details: event.details == null ? [] : event.details.map((detail) => ({
+            ...detail,
+            recipe: recipes.find((recipe) => recipe._id === detail.recipe),
+          })),
+        }
       }
     }
 
     return result
   },
 )
-
